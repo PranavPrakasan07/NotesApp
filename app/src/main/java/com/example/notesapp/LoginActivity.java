224d32ca28ed85f0ffa873399b8d5f1c72961587
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-05-05 00:08:36 +0530

    login signup UI

---
+++ app/src/main/java/com/example/notesapp/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.notesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    TextView signup_link;
+
+    Button login_button;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+        signup_link = findViewById(R.id.signup_link);
+
+        login_button = findViewById(R.id.login_button);
+
+        Intent intent = getIntent();
+
+        try {
+            String email_text = intent.getStringExtra("email");
+            String password_text = intent.getStringExtra("password");
+
+            email.setText(email_text);
+            password.setText(password_text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        signup_link.setOnClickListener(v -> {
+            Intent intent1 = new Intent(getApplicationContext(), SignUpActivity.class);
+
+            Bundle bundle = new Bundle();
+
+            String email_text = email.getText().toString();
+            String password_text = password.getText().toString();
+
+            bundle.putString("email", email_text);
+            bundle.putString("password", password_text);
+
+            intent1.putExtras(bundle);
+
+            startActivity(intent1);
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/notesapp/SignUpActivity.java
@@ -0,0 +1,62 @@
+package com.example.notesapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SignUpActivity extends AppCompatActivity {
+
+    EditText email, password, contact;
+    TextView login_link;
+
+    Button signup_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        contact = findViewById(R.id.contact_number);
+
+        login_link = findViewById(R.id.login_link);
+
+        signup_button = findViewById(R.id.signup_button);
+
+        Intent intent = getIntent();
+
+        try {
+            String email_text = intent.getStringExtra("email");
+            String password_text = intent.getStringExtra("password");
+
+            email.setText(email_text);
+            password.setText(password_text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        login_link.setOnClickListener(v -> {
+            Intent intent1 = new Intent(getApplicationContext(), LoginActivity.class);
+
+            Bundle bundle = new Bundle();
+
+            String email_text = email.getText().toString();
+            String password_text = password.getText().toString();
+
+            bundle.putString("email", email_text);
+            bundle.putString("password", password_text);
+
+            intent1.putExtras(bundle);
+
+            startActivity(intent1);
+        });
+    }
+
+}
